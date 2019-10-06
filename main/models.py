e8d1a553b3f1b1cd9dfeac5f6ccb9d5b01b549d6
wan <851798349@qq.com>
2019-10-06 17:08:55 +0800

    修改所有页面页脚

--- main/models.py
+++ main/models.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from django.db import models
 
 # Create your models here.
@@ -5,4 +7,16 @@ from django.db import models
 class user_acount(models.Model):
     uname = models.CharField(max_length=100, primary_key=True)
     upass = models.CharField(max_length=100)
+
+class foods(models.Model):
+    fid = models.AutoField(auto_created=1000, primary_key=True)
+    fname = models.CharField(max_length=255, unique=True)
+    price = models.CharField(max_length=50)
+    size = models.CharField(max_length=11)
+    number = models.IntegerField()
+    ftype = models.CharField(max_length=30)
+    img_path = models.CharField(max_length=50)
+
+
+
 
